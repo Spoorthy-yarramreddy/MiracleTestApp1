79f356fc605e6ffe18a9c51f2d224a61533445d6
Spoorthy-yarramreddy <yarramreddyspoorthy@gmail.com>
2024-01-15 14:41:33 -0600

    Initial commit

---
+++ src/main/java/com/example/demo/MiracleTestApplication1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiracleTestApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiracleTestApplication1Application.class, args);
+	}
+
+}
